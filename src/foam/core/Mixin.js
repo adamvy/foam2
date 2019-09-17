79d76684607c8456e9ded589e91ecfcaa46522a6
Adam Van Ymeren <adam@vany.ca>
2019-09-17 14:48:20 -0400

    Add Mixin

---
+++ src/foam/core/Mixin.js
@@ -0,0 +1,31 @@
+foam.CLASS({
+  package: 'foam.core',
+  name: 'Mixin',
+  properties: [
+    {
+      name: 'name'
+    },
+    [ 'priority', 150 ]
+  ],
+  methods: [
+    function installInClass(cls) {
+      cls.installModel(this.__context__.lookup(this.name).model_);
+    }
+  ]
+});
+
+foam.CLASS({
+  package: 'foam.core',
+  name: 'ModelMixinRefinement',
+  refines: 'Model',
+  properties: [
+    {
+      class: 'AxiomArray',
+      of: 'foam.core.Mixin',
+      name: 'mixins',
+      adaptArrayElement: function(e) {
+        return foam.core.Mixin.create({ name: e });
+      }
+    }
+  ]
+});
